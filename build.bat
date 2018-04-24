bd6b2902d94a60f07cf941b49d2f87d117589b90
kalinovcic <lovro.kalinovcic@gmail.com>
2018-04-24 21:28:47 +0200

    Optimized

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 setlocal
 cd %~dp0
 path=%~dp0MinGW\bin;%path%
-g++ src/titrica.cpp -o run_tree/titrica run_tree/SDL2.dll run_tree/glew32.dll -lopengl32 -lglu32 -std=c++11 --static
+g++ -O2 src/titrica.cpp -o run_tree/titrica run_tree/SDL2.dll run_tree/glew32.dll -lopengl32 -lglu32 -std=c++11 --static -Wl,-subsystem,windows
